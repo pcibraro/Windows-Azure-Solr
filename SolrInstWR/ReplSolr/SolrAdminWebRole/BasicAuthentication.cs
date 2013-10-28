dfb599fb7b6fedba7ee5de904a129b68e13de54f
pcibraro <pcibraro@hotmail.com>
2013-10-28 16:32:03 -0300

    Added missing files for basic auth

---
+++ SolrInstWR/ReplSolr/SolrAdminWebRole/BasicAuthentication.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.WindowsAzure.ServiceRuntime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SolrAdminWebRole
+{
+    public class BasicAuthentication : ActionFilterAttribute
+    {
+        string username;
+        string password;
+
+        public BasicAuthentication()
+        {
+            if (RoleEnvironment.IsAvailable)
+            {
+                var credentials = RoleEnvironment.GetConfigurationSettingValue("BasicAuthCredentials");
+                if (credentials != null)
+                {
+                    var usernameAndPassword = credentials.Split(';');
+                    username = usernameAndPassword[0];
+                    password = usernameAndPassword[1];
+                }
+            }
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!string.IsNullOrWhiteSpace(username) &&
+                !string.IsNullOrWhiteSpace(password))
+            {
+                var req = filterContext.HttpContext.Request;
+                if (!String.IsNullOrEmpty(req.Headers["Authorization"]))
+                {
+                    var credentials = System.Text.ASCIIEncoding.ASCII
+                                .GetString(Convert.FromBase64String(req.Headers["Authorization"].Substring(6)))
+                                .Split(':');
+                    var user = new { Name = credentials[0], Password = credentials[1] };
+                    if (!(user.Name == username && user.Password == password))
+                    {
+                        filterContext.Result = new HttpStatusCodeResult(401);
+                    }
+                }
+                else
+                {
+                     var host = filterContext.HttpContext.Request.Url.DnsSafeHost;                    
+                     filterContext.HttpContext.Response.Headers
+                        .Add("www-authenticate", string.Format("Basic realm=\"{0}\"", host));
+
+                     filterContext.Result = new HttpStatusCodeResult(401);
+                }
+            }
+        }
+    }
+}
---
+++ SolrInstWR/ReplSolr/SolrAdminWebRole/ContentStreamResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SolrAdminWebRole
+{
+    public class ContentStreamResult : ActionResult
+    {
+        public string ContentEncoding { get; set; }
+        public string ContentType { get; set; }
+        public Stream Response { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (!string.IsNullOrWhiteSpace(ContentEncoding))
+                context.HttpContext.Response.ContentEncoding = Encoding.GetEncoding(ContentEncoding);
+
+            if (!string.IsNullOrWhiteSpace(ContentType))
+                context.HttpContext.Response.ContentType = ContentType;
+
+            Response.CopyTo(context.HttpContext.Response.OutputStream);
+        }
+    }
+}
