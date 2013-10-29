cb611eab9565fa60cf3912e79877750fde890a26
pcibraro <pcibraro@hotmail.com>
2013-10-29 13:41:09 -0300

    Rolled back changes in delta sync

--- SolrInstWR/ReplSolr/SolrAdminWebRole/SyncJobsLogger.cs
+++ SolrInstWR/ReplSolr/SolrAdminWebRole/SyncJobsLogger.cs
@@ -1,59 +1,59 @@
-﻿//using Microsoft.WindowsAzure;
-//using Microsoft.WindowsAzure.StorageClient;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
-//namespace SolrAdminWebRole
-//{
-//    public class SyncJobsLogger
-//    {
-//        const string SyncResulsTableName = "SyncJobs";
+namespace SolrAdminWebRole
+{
+    public class SyncJobsLogger
+    {
+        const string SyncResulsTableName = "SyncJobs";
 
-//        string connectionString;
+        string connectionString;
 
-//        public SyncJobsLogger(string connectionString)
-//        {
-//            this.connectionString = connectionString;
-//        }
+        public SyncJobsLogger(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
 
-//        public void CreateTableIfNotExists()
-//        {
-//            var account = CloudStorageAccount.Parse(this.connectionString);
-//            account.CreateCloudTableClient().CreateTableIfNotExist(SyncResulsTableName);
-//        }
+        public void CreateTableIfNotExists()
+        {
+            var account = CloudStorageAccount.Parse(this.connectionString);
+            account.CreateCloudTableClient().CreateTableIfNotExist(SyncResulsTableName);
+        }
 
-//        public bool SaveJob(bool status, string description, string url)
-//        {
-//            var context = CloudStorageAccount.Parse(this.connectionString)
-//                .CreateCloudTableClient()
-//                .GetDataServiceContext();
+        public bool SaveJob(bool status, string description, string url)
+        {
+            var context = CloudStorageAccount.Parse(this.connectionString)
+                .CreateCloudTableClient()
+                .GetDataServiceContext();
 
-//            context.AddObject(SyncResulsTableName, new SyncJob
-//                {
-//                    Date = DateTime.UtcNow,
-//                    Status = status,
-//                    Url = url,
-//                    PartitionKey = "Delta",
-//                    Description = description,
-//                    RowKey = string.Format("{0:d10}", DateTime.MaxValue.Ticks
-//                        - DateTime.UtcNow.Ticks),
+            context.AddObject(SyncResulsTableName, new SyncJob
+                {
+                    Date = DateTime.UtcNow,
+                    Status = status,
+                    Url = url,
+                    PartitionKey = "Delta",
+                    Description = description,
+                    RowKey = string.Format("{0:d10}", DateTime.MaxValue.Ticks
+                        - DateTime.UtcNow.Ticks),
 
-//                });
+                });
 
-//            context.SaveChangesWithRetries();
+            context.SaveChangesWithRetries();
 
-//            return true;
+            return true;
 
-//        }
+        }
 
-//        internal class SyncJob : TableServiceEntity
-//        {
-//            public DateTime Date { get; set; }
-//            public bool Status { get; set; }
-//            public string Description { get; set; }
-//            public string Url { get; set; }
-//        }
-//    }
-//}
+        internal class SyncJob : TableServiceEntity
+        {
+            public DateTime Date { get; set; }
+            public bool Status { get; set; }
+            public string Description { get; set; }
+            public string Url { get; set; }
+        }
+    }
+}
